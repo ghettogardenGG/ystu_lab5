514ac953680711f245fafbcaaee778ab77c26a43
ghettogardenGG <supp.mad.max@yandex.ru>
2023-05-21 15:08:41 +0300

    Добавление простого калькулятора

---
+++ testApp/testApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой консольный калькулятор");
+
+        Console.Write("Введите первое число: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите операцию (+, -, *, /): ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            default:
+                Console.WriteLine("Неверная операция");
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+
+        // Коммит изменений в локальный репозиторий
+        // git commit -am "Добавлен простой калькулятор"
+
+        // Запушить изменения в удалённый репозиторий
+        // git push origin master
+    }
+}
